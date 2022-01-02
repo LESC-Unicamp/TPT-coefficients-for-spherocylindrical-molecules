4d78d666b0780d851018df0936346534dfc5b8da
Nathan Barros de Souza <73966482+nathanbsouza@users.noreply.github.com>
2022-01-02 19:08:49 -0300

    Update module_global_variables.f95
    Version 1.0.0

--- module_global_variables.f95
+++ module_global_variables.f95
@@ -157,7 +157,7 @@ module globalvar
 	logical, dimension (3)	:: config_selec		! Checks the selected molecular configuration
 	logical, dimension (6)	:: fexist		! Checks whether folder exists or not
 	logical, dimension (7)	:: dfexist		! Checks whether date folders exist or not
-	logical, dimension (3)	:: sfexist		! Checks whether subfolder exists or not
+	logical, dimension (7)	:: sfexist		! Checks whether subfolder exists or not
 
 	! ***************************************************************************************
 	! Logical variables (allocatable)
